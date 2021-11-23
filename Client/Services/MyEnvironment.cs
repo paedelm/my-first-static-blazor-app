6d2e7899723c897279d34528d095ad1227810b50
Peter Edelman <paedelm@gmail.com>
2021-11-23 15:07:26 +0100

    Environment via service

---
+++ Client/Services/MyEnvironment.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace BlazorApp.Client.Services
+{
+    public class MyEnvironment
+    {
+        public MyEnvironment(IWebAssemblyHostEnvironment hostEnvironment)
+        {
+            HostEnvironment=hostEnvironment;
+        }
+
+        public IWebAssemblyHostEnvironment HostEnvironment { get; }
+    }
+}
