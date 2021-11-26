1bd09c2781148245f7f586444164ef22d5fe2119
paedelm <paedelm@gmail.com>
2021-11-26 13:56:35 +0100

    AuthorizationLevel.Function

--- Api/TestFunc.cs
+++ Api/TestFunc.cs
@@ -20,7 +20,7 @@ namespace BlazorApp.Api
     {
         [FunctionName("TestFunc")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.User, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request for function TestFunc.");
