6717ca3f34c3be78f24a4e11240e534229ff6d7b
paedelm <paedelm@gmail.com>
2021-11-27 10:13:11 +0100

    Use [Authorize] and ClaimsPrincipal on Azure Function Run

---
+++ Api/StaticWebAppsAuth.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.ComponentModel.DataAnnotations;
+#nullable enable
+namespace BlazorApp.Api
+{
+    public static class StaticWebAppsAuth
+    {
+        private class ClientPrincipal
+        {
+            public string? IdentityProvider { get; init; }
+            public string? UserId { get; init; }
+            public string? UserDetails { get; init; }
+            public IEnumerable<string>? UserRoles { get; set; }
+        }
+
+        public static ClaimsPrincipal Parse(HttpRequest req)
+        {
+            var principal = new ClientPrincipal();
+
+            if (req.Headers.TryGetValue("x-ms-client-principal", out var header))
+            {
+                var data = header[0];
+                var decoded = Convert.FromBase64String(data);
+                var json = Encoding.UTF8.GetString(decoded);
+                principal = System.Text.Json.JsonSerializer.Deserialize<ClientPrincipal>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+            }
+
+            principal.UserRoles = principal.UserRoles?.Except(new string[] { "anonymous" }, StringComparer.CurrentCultureIgnoreCase);
+
+            if ((!principal.UserRoles?.Any() ?? true) || principal.UserId == null)
+            {
+                return new ClaimsPrincipal();
+            }
+
+            var identity = new ClaimsIdentity(principal.IdentityProvider);
+            identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, principal.UserId));
+            identity.AddClaim(new Claim(ClaimTypes.Name, principal.UserDetails ?? "NoUserDetails"));
+            identity.AddClaims(principal.UserRoles.Select(r => new Claim(ClaimTypes.Role, r)));
+
+            return new ClaimsPrincipal(identity);
+        }
+        public static ObjectResult Forbidden(string? errorMessage = "Error 403 Forbidden")
+        {
+            return new ObjectResult(errorMessage)
+            {
+                StatusCode = (int?)HttpStatusCode.Forbidden
+            };
+
+        }
+    }
+}
--- Api/TestFunc.cs
+++ Api/TestFunc.cs
@@ -8,24 +8,22 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using BlazorApp.Shared;
-using System.Collections.Generic;
-using System.Security.Claims;
-using System.Text;
-using System.Text.Json;
-using System.Linq;
 using System.Net;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace BlazorApp.Api
 {
     public static class TestFunc
     {
+        [Authorize]
         [FunctionName("TestFunc")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ClaimsPrincipal principal,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request for function TestFunc.");
-            var principal = StaticWebAppsAuth.Parse(req);
+            //var principal = StaticWebAppsAuth.Parse(req);
             var identName = principal.Identity == null ? "anonymous" : principal.Identity.Name;
             log.LogInformation($"principal: {identName}");
             string name = req.Query["name"];
@@ -37,54 +35,18 @@ namespace BlazorApp.Api
             var responseMessage = string.IsNullOrEmpty(name)
                 ? new NaamBericht(Naam: "Nobody", Bericht: $"{kenmerk} principal:{identName}:This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.")
                 : new NaamBericht(Naam: name, Bericht: $"{kenmerk} principal: {identName}:Hello, {name}. This HTTP triggered function executed successfully.");
-            if (identName != "anonymous" || kenmerk.StartsWith("DEVL"))
+            if (principal.Identity != null || kenmerk.StartsWith("DEVL"))
             {
                 return new OkObjectResult(responseMessage);
             }
             else
             {
-                return new ObjectResult("Error 403 Forbidden")
-                {
-                    StatusCode = (int?)HttpStatusCode.Forbidden
-                };
+                return StaticWebAppsAuth.Forbidden(errorMessage: "Fout 403: Niet Toegestaan");
+                //return new ObjectResult("Error 403 Forbidden")
+                //{
+                //    StatusCode = (int?)HttpStatusCode.Forbidden
+                //};
             }
-        }
-    }
-    public static class StaticWebAppsAuth
-    {
-        private class ClientPrincipal
-        {
-            public string IdentityProvider { get; set; }
-            public string UserId { get; set; }
-            public string UserDetails { get; set; }
-            public IEnumerable<string> UserRoles { get; set; }
-        }
-
-        public static ClaimsPrincipal Parse(HttpRequest req)
-        {
-            var principal = new ClientPrincipal();
-
-            if (req.Headers.TryGetValue("x-ms-client-principal", out var header))
-            {
-                var data = header[0];
-                var decoded = Convert.FromBase64String(data);
-                var json = Encoding.UTF8.GetString(decoded);
-                principal = System.Text.Json.JsonSerializer.Deserialize<ClientPrincipal>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-            }
-
-            principal.UserRoles = principal.UserRoles?.Except(new string[] { "anonymous" }, StringComparer.CurrentCultureIgnoreCase);
-
-            if (!principal.UserRoles?.Any() ?? true)
-            {
-                return new ClaimsPrincipal();
-            }
-
-            var identity = new ClaimsIdentity(principal.IdentityProvider);
-            identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, principal.UserId));
-            identity.AddClaim(new Claim(ClaimTypes.Name, principal.UserDetails));
-            identity.AddClaims(principal.UserRoles.Select(r => new Claim(ClaimTypes.Role, r)));
-
-            return new ClaimsPrincipal(identity);
         }
     }
 }
